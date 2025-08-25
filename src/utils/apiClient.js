4920c31839cea23d42223554eaecb32de975e8cc
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-08-25 22:05:21 +0200

    feat(apiClient): add API fetch function
    API function to fetch data with arguments choose endpoint, options and auth. #5

---
+++ src/utils/apiClient.js
@@ -0,0 +1,39 @@
+const API_BASE = 'https://v2.api.noroff.dev';
+
+export const apiClient = async (endpoint, options = {}, requireAuth = false) =>{
+    const token = localStorage.getItem('accessToken');
+    
+    if (requireAuth && !token) {
+        throw new Error('Authentication required but no token found.');
+    }
+
+    const headers = {
+        'Content-Type': 'application/json',
+        ...(options.headers || {}),
+    };
+
+    if (requireAuth) {
+        headers['Authorization'] = `Bearer ${token}`;
+    }
+    
+    const config = {
+        ...options,
+        method: options.method || 'GET',
+        headers,
+    }
+
+    if (config.body && typeof config.body === 'object') {
+        config.body = JSON.stringify(config.body);
+    }
+
+
+    const response = await fetch(`${API_BASE}${endpoint}`, config);
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.errors?.[0]?.message || 'API request failed. Try again');
+    }
+
+    return data;
+}
