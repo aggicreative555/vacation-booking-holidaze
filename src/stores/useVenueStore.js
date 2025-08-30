0712376e213338cd6a0526c83f6fbaadc84574f3
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-08-30 09:13:44 +0200

    feat(booking/id): add store to accomodate for booking/id api request
    add fetchbyid store that uses existing and new data to find the information about a venue based on id #20

--- src/stores/useVenueStore.js
+++ src/stores/useVenueStore.js
@@ -1,8 +1,9 @@
 import { create } from 'zustand';
 import { apiClient } from '../utils/apiClient';
 
-export const useVenueStore = create((set) => ({
+export const useVenueStore = create((set, get) => ({
   venue: [],
+  singleVenue: null,
   isLoading: false,
   isError: false,
 
@@ -11,8 +12,29 @@ export const useVenueStore = create((set) => ({
 
     try {
       const data = await apiClient('/holidaze/venues');
-      console.log(data);
-      set({ venue: data.data, isLoading: false });
+      set({ venue: data.data || [], isLoading: false });
+    } catch (error) {
+      console.error('Fetch error:', error);
+      set({ isError: true, isLoading: false, venue: [] });
+    }
+  },
+
+  fetchVenueById: async (id) => {
+    set({ isLoading: true, isError: false });
+
+    const existing = Array.isArray(get().venue)
+    ? get().venue.find((v) => v.id === id) 
+    : null;
+    
+    if (existing) {
+        set({ singleVenue: existing, isLoading: false});
+        return existing;
+    }
+
+
+    try {
+      const data = await apiClient(`/holidaze/venues/${id}`);
+      set({ singleVenue: data.data, isLoading: false });
     } catch (error) {
       console.error('Fetch error:', error);
       set({ isError: true, isLoading: false });
