08fa51e97622e8afc0e1a799cc8126235ef5ec11
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-09-19 23:56:31 +0200

    fix(singlevenue): fix condition, fix calendar overlap
    fixed conditions
    added an overlapping, exceedsGuests and canBook condition for calendar error handling and making a booking #34

--- src/components/venues/SingleVenue.jsx
+++ src/components/venues/SingleVenue.jsx
@@ -9,75 +9,81 @@ import { useAuthStore } from '../../stores/useAuthStore';
 import Modal from '../modal/Modal';
 import EditVenueForm from '../forms/EditVenueForm';
 import { showToast } from '../../utils/toast';
-import { Calendar, Minus, Plus } from 'lucide-react';
+import { Minus, Plus } from 'lucide-react';
 import CalendarPicker from '../calendar/CalendarPicker';
 
 function SingleVenue() {
   const { id } = useParams();
   const { singleVenue, isLoading, isError, fetchVenueById } = useVenueStore();
-  const { addToBookings, bookings, removeFromBookings } = useBookingStore();
+  const { addToBookings, userBookings, removeFromBookings } = useBookingStore();
   const { user } = useAuthStore();
   const navigate = useNavigate();
   const [editIsOpen, setEditIsOpen] = useState(false);
   const [selectedRange, setSelectedRange] = useState(null);
   const [guests, setGuests] = useState(1);
   const [calculatedPrice, setCalculatedPrice] = useState(0);
-  const canBook = selectedRange?.from && selectedRange?.to && !exisitingBooking;
 
-  const exisitingBooking = bookings.find(
-    (b) => b.venue?.id === singleVenue?.id
-  );
+    const hasOverlap = userBookings.some((b) =>
+        b.venue?.id === singleVenue?.id &&
+        selectedRange?.from <= new Date(b.dateTo) &&
+        selectedRange?.to >= new Date(b.dateFrom)
+    );
 
-  const handleAddBooking = () => {
+    const exceedsGuests = guests > singleVenue?.maxGuests;
+
+    const canBook = selectedRange?.from && selectedRange?.to && !hasOverlap && !exceedsGuests;
+
+
+    const handleAddBooking = () => {
     if (!selectedRange?.from || !selectedRange.to) {
-      return showToast.error('Please select a date for your vacation');
+        return showToast.error('Please select a date for your vacation');
     }
 
     addToBookings(
-      singleVenue,
-      {
+        singleVenue,
+        {
         dateFrom: selectedRange.from,
         dateTo: selectedRange.to,
         guests: guests,
-      },
-      user?.name
+        },
+        user?.name
     );
-  };
+    };
 
-  const handleCancelBooking = () => {
-    if (!exisitingBooking) return;
-    removeFromBookings(exisitingBooking.id);
-  };
+    const handleCancelBooking = () => {
+    if (!hasOverlap) return;
+    removeFromBookings(userBookings.id);
+    };
 
-  useEffect(() => {
+    useEffect(() => {
     if (selectedRange?.from && selectedRange?.to) {
-      const nights =
+        const nights =
         (selectedRange?.to - selectedRange?.from) / (1000 * 60 * 60 * 24);
-      setCalculatedPrice(singleVenue?.price * nights);
+        setCalculatedPrice(singleVenue?.price * nights);
     }
-  }, [selectedRange, singleVenue?.price]);
+    }, [selectedRange, singleVenue?.price]);
 
-  const incrementGuests = () => {
+    const incrementGuests = () => {
     if (guests < singleVenue.maxGuests) {
-      setGuests((prev) => prev + 1);
+        setGuests((prev) => prev + 1);
     }
-  };
+    };
 
-  const decrementGuests = () => {
+    const decrementGuests = () => {
     if (guests > 1) {
-      setGuests((prev) => prev - 1);
+        setGuests((prev) => prev - 1);
     }
-  };
+    };
 
-  useEffect(() => {
+    useEffect(() => {
     if (!singleVenue || singleVenue.id !== id) {
-      fetchVenueById(id);
+        fetchVenueById(id);
     }
-  }, [id, singleVenue, fetchVenueById]);
+    }, [id, singleVenue, fetchVenueById]);
 
-  if (isLoading) return <p>Loading Content...</p>;
-  if (isError) return <p>Error loading venues, please refresh the page...</p>;
-  if (!singleVenue) return <NotFound />;
+    if (isLoading) return <p>Loading Content...</p>;
+    if (isError) return <p>Error loading venues, please refresh the page...</p>;
+    if (!singleVenue) return <NotFound />;
 
   return (
     <>
@@ -126,6 +132,7 @@ function SingleVenue() {
                 {singleVenue.location.country}
               </p>
             </div>
+            {/* Venue manager (edit venue) */}
             {user?.venueManager && singleVenue?.owner?.name === user?.name && (
               <div className="flex gap-6 flex-row justify-between w-full">
                 <button
@@ -164,9 +171,11 @@ function SingleVenue() {
               </div>
             )}
 
-            {!user?.venueManager && user !== null && !exisitingBooking && (
+            {/* CUSTOMER BOOKING FLOW */}
+            {user && !user.venueManager && (
               <div className="flex gap-6 flex-col max-w-[500px] justify-between w-full">
                 <CalendarPicker onSelectRange={setSelectedRange} />
+
                 <div className="flex items-center gap-4 mt-6 w-[500px]">
                   <p className="font-lg font-caslon uppercase text-gray-400">
                     Guests
@@ -178,15 +187,13 @@ function SingleVenue() {
                   >
                     <Minus />
                   </button>
-                  <label className="label-base">
-                    <input
-                      type="number"
-                      min={1}
-                      value={guests}
-                      onChange={(e) => setGuests(Number(e.target.value))}
-                      className="input-base border-b border-gray-300 focus:border-black transition-all text-xl justify-center items-center flex"
-                    />
-                  </label>
+                  <input
+                    type="number"
+                    min={1}
+                    value={guests}
+                    onChange={(e) => setGuests(Number(e.target.value))}
+                    className="input-base border-b border-gray-300 focus:border-black transition-all text-xl text-center"
+                  />
                   <button
                     onClick={incrementGuests}
                     disabled={guests >= singleVenue.maxGuests}
@@ -195,57 +202,46 @@ function SingleVenue() {
                     <Plus />
                   </button>
                 </div>
-                <div className="text-3xl md:text-5xl text-center font-garamond uppercase tracking-tighter text-red-800 transition-all duration-300 ease-in-out">
+
+                <div className="text-3xl md:text-5xl text-center font-garamond uppercase tracking-tighter text-red-800">
                   {calculatedPrice} NOK total
                 </div>
-                <button
-                  className="btn-l btn-primary w-full"
-                  onClick={handleAddBooking}
-                  disabled={!canBook && !exisitingBooking}
-                >
-                  Book now
-                </button>
+
+                {!hasOverlap ? (
+                  <button
+                    className="btn-l btn-primary w-full"
+                    onClick={handleAddBooking}
+                    disabled={!canBook}
+                  >
+                    Book now
+                  </button>
+                ) : (
+                  <button
+                    className="btn-l btn-secondary w-full"
+                    onClick={handleCancelBooking}
+                  >
+                    Cancel booking
+                  </button>
+                )}
+
                 <button
                   className="btn-l btn-secondary w-full"
-                  onClick={() => {
-                    navigate(-1);
-                  }}
+                  onClick={() => navigate(-1)}
                 >
                   Go back
                 </button>
               </div>
             )}
 
-            {!user?.venueManager && user !== null && exisitingBooking && (
-              <div>
-                <button
-                  className="btn-l btn-primary w-full disabled:bg-gray-300 disabled:text-gray-500"
-                  onClick={handleCancelBooking}
-                  disabled={!canBook && !exisitingBooking}
-                >
-                  Cancel booking
-                </button>
-                <button
-                  className="btn-l btn-secondary w-full"
-                  onClick={() => {
-                    navigate(-1);
-                  }}
-                >
-                  Go back
-                </button>
-              </div>
-            )}
-
-            {user === null && (
+            {/* NOT LOGGED IN */}
+            {!user && (
               <div className="flex gap-6 flex-col max-w-[300px] justify-between w-full">
                 <Link className="btn-l btn-primary w-full" to="/login">
                   Log in to book venue
                 </Link>
                 <button
                   className="btn-l btn-secondary w-full"
-                  onClick={() => {
-                    navigate(-1);
-                  }}
+                  onClick={() => navigate(-1)}
                 >
                   Go back
                 </button>
