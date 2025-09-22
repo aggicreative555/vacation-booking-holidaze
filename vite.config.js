fc17ba6fe4b1f604a4b2fa0561362512cdf969a7
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-09-22 20:42:42 +0200

    fix(vite): attempt to fix vite error
    'Uncaught SyntaxError: Identifier '__vite__injectQuery' has already been declared (at ${mod.id}:33159:1)'
    
    duplicated HMR code.
    
    what actions didnt work:
    updating vite, node and all packages
    removing and reinstalling all vite related files
    removing cache
    hard reset on browser
    removing fast refresh (HMR responsible)
    #42

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react({ fastRefresh: false })],
   publicDir: 'public',
   build: {
     outDir: 'build',
+  },
+  optimizeDeps: {
+    exclude: ['lightningcss'],
+  },
+  ssr: {
+    noExternal: ['lightningcss'],
   },
 });
